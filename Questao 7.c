c85bf4d83047d5d57ec023fad733245fcc2b63a9
Kaique Gomes Machado <kaiquered@hotmail.com>
2019-08-28 18:28:36 -0300

    lista 1

--- Questao 7.c
+++ Questao 7.c
@@ -1,5 +1,4 @@
 /*  
-
-     *(pulo + 2), pois um vetor È inicializado com Ìndice 0, utilizando o operador * acessamos o valor na posiÁ„o 0 do vetor
-     como est· incrementado por 2, iremos acessar o valor da posiÁ„o 3.
-*/
+     *(pulo + 2), pois um vetor √© inicializado com √≠ndice 0, utilizando o operador * acessamos o valor na posi√ß√£o 0 do vetor
+     como est√° incrementado por 2, iremos acessar o valor da posi√ß√£o 3.
+*/ 
